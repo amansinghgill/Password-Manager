66da648d8752e4740f153e20a3c1122a441b32f9
Aman <gillaman1998@gmail.com>
2023-10-31 18:25:40 -0400

    save function added

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from tkinter import *
 
+
+#Save Password Function
+
+def save():
+
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    with open("data.text", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password}\n")
+        website_entry.delete(0, END)
+        email_entry.delete(0,END)
+        password_entry.delete(0,END)
+
+#UI
+
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
 
 canvas = Canvas(height=200, width=200)
-logo_img = PhotoImage(file="logo.png")  # Make sure the logo.png file is in the same directory as this script
+logo_img = PhotoImage(file="logo.png")  
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
@@ -20,15 +37,17 @@ password_label.grid(row=3, column=0, sticky='E')  # Aligned to the right (east)
 # Entries
 website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=2, sticky='EW')  # Aligned to both sides (east and west)
+website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2, sticky='EW')  # Aligned to both sides (east and west)
+#email_entry.insert(END)
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1, sticky='EW')  # Aligned to both sides (east and west)
 
 # Buttons
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=2, sticky='EW')  # Aligned to both sides (east and west)
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2, sticky='EW')  # Aligned to both sides (east and west)
 
 # Ensuring that the columns expand proportionally when the window is resized
